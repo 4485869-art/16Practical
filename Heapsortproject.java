96f1b3f789dc993b8c64911b4dc5fee2c216300f
4485869-art <4485869@myuwc.ac.za>
2026-03-18 12:50:13 +0000

    Created main merthod

---
+++ Heapsortproject.java
@@ -0,0 +1,9 @@
+#HEAP.JAVA
+// Themba Gift Sithole
+// 4485869
+// Practical 6 - Heapsort
+// Consulted: Calude: Free version (For better and deep breakdown)
+
+public class Heapsortproject {
+    private String[] heap;
+    private int size;
